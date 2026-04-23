178691077f5152d20af071c493d241e097e65a04
Horst507 <lucaneidhart@gmx.de>
2026-04-23 13:26:35 +0200

    Implement calendar view and enhance UI with new templates and styles

--- src/server/webapp/frontend/ui/views.py
+++ src/server/webapp/frontend/ui/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render,HttpResponse
+import calendar
+from datetime import datetime
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Hello World!")
+    return render(request, "test.html")
+
+
+def calendar_view(request):
+    now = datetime.now()
+
+    year = now.year
+    month = now.month
+
+    # Wochentag des 1. Tages (0=Montag)
+    start_weekday, num_days = calendar.monthrange(year, month)
+
+    context = {
+        "current_year": year,
+        "current_month": now.strftime("%B"),
+        "current_month_number": month,
+        "month_days": list(range(1, num_days + 1)),
+        "start_weekday_range": range(start_weekday),
+        "jobs": None#Job.objects.all()
+    }
+
+    return render(request, "calendar.html", context)
