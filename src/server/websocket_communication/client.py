9bddcc93e4738cab1be59dc5aedc97783e0fb129
Veicm <180028722+Veicm@users.noreply.github.com>
2026-05-04 19:00:27 +0200

    Implement code changes to enhance functionality and improve performance

--- src/server/websocket_communication/client.py
+++ src/server/websocket_communication/client.py
@@ -1,5 +1,7 @@
+from typing import cast
 from websockets.sync.client import connect
-from .helper.types import Request, Response
+from helper.types import Request, Response
+
 
 class WebSocketClient:
     def __init__(self):
@@ -16,20 +18,17 @@ class WebSocketClient:
         with connect(uri) as weboscket:
             weboscket.send(data)
 
-
-    def receive_data(self, uri: str) -> Respones:
+    def receive_data(self, uri: str) -> Response:
         """
         Receives data from uri ( see init )
         via websockets
         """
         with connect(uri) as websocket:
-            data=websocket.recv()
-        return data
+            raw_data: str | bytes = websocket.recv()
+        return cast(Response, raw_data)
 
     def sending(self, data: Request, ip: str) -> Response:
         uri: str = f"ws://{ip}:{self.port}"
 
         self.send_data(data, uri)
         return self.receive_data(uri)
-
-
