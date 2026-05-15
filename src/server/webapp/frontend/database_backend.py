05405852df70aaf1a0d44e909a50c2076e8df54f
Horst507 <lucaneidhart@gmx.de>
2026-05-16 01:34:33 +0200

    Enhance job and task management: add Task model, update Job model fields, and create task management UI !!! WORK IN PROGRESS !!!

--- src/server/webapp/frontend/database_backend.py
+++ src/server/webapp/frontend/database_backend.py
@@ -4,11 +4,11 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "frontend.settings")
 django.setup()
 
-from ui.models import Job, Client
+from ui.models import Job, Client, Task
 
 class DatabaseBackend:
     def __init__(self):
-        pass
+        print()
     def get_clients(self):
 
         self.clients = list(Client.objects.all().values_list())
@@ -18,5 +18,6 @@ class DatabaseBackend:
     def get_jobs(self):
         self.jobs = list(Job.objects.all().values_list())
         return self.jobs
-    
-    
+    def get_tasks(self):
+        self.task = list(Task.objects.all().values_list())
+        return self.task
--- src/server/webapp/frontend/ui/models.py
+++ src/server/webapp/frontend/ui/models.py
@@ -2,19 +2,26 @@ from django.db import models
 
 class Job(models.Model):
     id = models.CharField(max_length=255, primary_key=True)
-    schedule = models.CharField(max_length=255)
+    
     enabled = models.BooleanField(default=True)
+    
+    client_id = models.CharField(max_length=255, null=False, blank=True)
+
+    task_id = models.CharField(max_length=255, null=False, blank=True)
+
+    time_to_run = models.DateTimeField(null=False, blank=True,default=None)
 
     last_run = models.DateTimeField(null=True, blank=True)
+    
     last_task_status = models.CharField(max_length=50, null=True, blank=True)
-    active_task_id = models.CharField(max_length=255, null=True, blank=True)
 
 
 class Task(models.Model):
     id = models.CharField(max_length=255, primary_key=True)
-    job = models.ForeignKey(Job, on_delete=models.CASCADE)
-
-    status = models.CharField(max_length=50)
+    #job = models.ForeignKey(Job, on_delete=models.CASCADE)
+    job_type = models.CharField(max_length=255,default = None, null=True, blank=True)
+    parameters = models.CharField(default=None, null=True, blank=True)
+    
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
